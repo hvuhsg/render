8ef68bd65e939a70163158c407882f733db72f47
Yehoyada <yehoyada@ditto.fit>
2025-06-07 14:31:43 +0300

    Use of offset to mimic sub canvas

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -11,37 +11,47 @@ import (
 var ErrOutOfBounds = errors.New("object is trying to be painted out of bounds")
 
 type Canvas struct {
-	Img  *image.RGBA
-	Size types.Size
+	Img    *image.RGBA
+	Size   types.Size
+	offset image.Point
 }
 
 func NewCanvas(size types.Size) *Canvas {
 	img := image.NewRGBA(image.Rect(0, 0, size.Width, size.Height))
 	return &Canvas{
-		Img:  img,
-		Size: size,
+		Img:    img,
+		Size:   size,
+		offset: image.Point{X: 0, Y: 0},
 	}
 }
 
 func (c *Canvas) SubCanvas(x, y int, size types.Size) *Canvas {
 	return &Canvas{
-		Img:  c.Img.SubImage(image.Rect(x, y, x+size.Width, y+size.Height)).(*image.RGBA),
-		Size: size,
+		Img:    c.Img,
+		Size:   size,
+		offset: image.Point{X: c.offset.X + x, Y: c.offset.Y + y},
 	}
 }
 
 func (c *Canvas) set(x, y int, color color.RGBA) {
 	c.assertPointInBounds(x, y)
-	c.Img.Set(x, y, color)
+	c.Img.Set(c.offset.X+x, c.offset.Y+y, color)
+}
+
+func (c *Canvas) get(x, y int) color.Color {
+	c.assertPointInBounds(x, y)
+	return c.Img.At(c.offset.X+x, c.offset.Y+y)
 }
 
 func (c *Canvas) assertPointInBounds(x, y int) {
-	inBounds := x >= 0 && x <= c.Size.Width && y >= 0 && y <= c.Size.Height
+	inBounds := x >= 0 && x < c.Size.Width && y >= 0 && y < c.Size.Height
 	if !inBounds {
 		panic(ErrOutOfBounds)
 	}
 }
 
+// Should only be used for drawing an already rendered canvas onto this canvas
+// When rendering child render objects, use SubCanvas instead
 func (c *Canvas) DrawCanvas(other *Canvas, x, y int) {
 	// Check if the other canvas would be drawn out of bounds
 	c.assertPointInBounds(x, y)
@@ -50,7 +60,7 @@ func (c *Canvas) DrawCanvas(other *Canvas, x, y int) {
 	// Draw each pixel from the other canvas onto this canvas
 	for i := range other.Size.Width {
 		for j := range other.Size.Height {
-			c.Img.Set(x+i, y+j, other.Img.At(i, j))
+			c.set(x+i, y+j, other.get(i, j).(color.RGBA))
 		}
 	}
 }

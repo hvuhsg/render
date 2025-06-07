25505b7a3cdd532f30114e90625cffbeebd13ead
Yehoyada <yehoyada@ditto.fit>
2025-06-07 03:36:45 +0300

    fix example

--- examples/basic_shapes/main.go
+++ examples/basic_shapes/main.go
@@ -16,7 +16,7 @@ func main() {
 	canvas.Circle(400, 300, 100, cv.Red, true)           // Filled red circle
 	canvas.Circle(400, 300, 120, cv.Blue, false)         // Blue circle outline
 	canvas.Rectangle(200, 200, 100, 100, cv.Green, true) // Filled green square
-	canvas.Line(0, 0, 800, 600, cv.Yellow, 5)            // Yellow diagonal line
+	canvas.Line(3, 3, 796, 596, cv.Yellow, 5)            // Yellow diagonal line (inset to avoid out-of-bounds)
 
 	// Save the result
 	file, _ := os.Create("basic_shapes.png")

d75adeb13ff8bb7243dbe8d7dfddaeeb2040342a
Yehoyada <yehoyada@ditto.fit>
2025-06-07 17:33:39 +0300

    update main example

--- README.md
+++ README.md
@@ -41,31 +41,34 @@ go get github.com/hvuhsg/render
 package main
 
 import (
-    "github.com/hvuhsg/render/canvas"
-    "github.com/hvuhsg/render/render_objects"
-    "github.com/hvuhsg/render/types"
+	"image/png"
+	"os"
+
+	"github.com/hvuhsg/render/canvas"
+	"github.com/hvuhsg/render/render_objects"
+	"github.com/hvuhsg/render/types"
 )
 
 func main() {
-    // Create a new canvas
-    canvas := canvas.NewCanvas(types.Size{Width: 800, Height: 600})
+	// Create a new canvas
+	canvas_ := canvas.NewCanvas(types.Size{Width: 800, Height: 600}, false)
 
-    // Create a text element
-    text := render_objects.NewText("Hello, World!", canvas.LightGreen, 36, "default")
+	// Create a text element
+	text := render_objects.NewText("Hello, World!", canvas.Purple, 36, "default")
 
-    // Center the text
-    align := &render_objects.Align{
-        Child: text,
-        Align: render_objects.AlignCenter,
-    }
+	// Center the text
+	align := &render_objects.Align{
+		Child: text,
+		Align: render_objects.AlignCenter,
+	}
 
-    // Render and save
-    align.Paint(canvas)
+	// Render and save
+	align.Paint(canvas_)
 
-    // Save the result
+	// Save the result
 	file, _ := os.Create("result.png")
 	defer file.Close()
-	png.Encode(file, canvas.Img)
+	png.Encode(file, canvas_.Img)
 }
 ```
 
--- cmd/main.go
+++ cmd/main.go
@@ -4,58 +4,29 @@ import (
 	"image/png"
 	"os"
 
-	cv "github.com/hvuhsg/render/canvas"
+	"github.com/hvuhsg/render/canvas"
 	"github.com/hvuhsg/render/render_objects"
 	"github.com/hvuhsg/render/types"
 )
 
-func saveToFile(canvas *cv.Canvas, filename string) error {
-	img := canvas.Img
-
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	png.Encode(file, img)
-
-	return nil
-}
-
 func main() {
-	canvas := cv.NewCanvas(types.Size{Width: 800, Height: 600}, false)
-	renderTree := createRenderTree()
-	renderTree.Paint(canvas)
-	saveToFile(canvas, "out.png")
-}
+	// Create a new canvas
+	canvas_ := canvas.NewCanvas(types.Size{Width: 800, Height: 600}, false)
 
-func createRenderTree() render_objects.RenderObject {
-	// Create a column of text elements with some spacing
-	box := &render_objects.ColoredBox{
-		Color:  cv.Red,
-		Width:  100,
-		Height: 100,
-	}
+	// Create a text element
+	text := render_objects.NewText("Hello, World!", canvas.Purple, 36, "default")
 
-	padding := &render_objects.Padding{
-		Child:  box,
-		Top:    100,
-		Left:   100,
-		Right:  100,
-		Bottom: 100,
-	}
-
-	column := &render_objects.Column{
-		Children: []render_objects.RenderObject{padding, box},
-		Sizing:   types.MainAxisSizeMin,
-	}
-
-	// Center everything in the canvas
+	// Center the text
 	align := &render_objects.Align{
-		Child: column,
-		Align: render_objects.AlignTopLeft,
+		Child: text,
+		Align: render_objects.AlignCenter,
 	}
 
-	return align
+	// Render and save
+	align.Paint(canvas_)
+
+	// Save the result
+	file, _ := os.Create("result.png")
+	defer file.Close()
+	png.Encode(file, canvas_.Img)
 }
